38efd2ca78fbf538dcc5ba668e905b02e9b24d96
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-03-05 17:26:29 -0500

    added post model with passing test

--- myblog/blogengine/models.py
+++ myblog/blogengine/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	pub_date = models.DateTimeField()
+	text = models.TextField()
