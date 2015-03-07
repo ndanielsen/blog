0dd6e85c51cd148d6914905921fed7aca54318f6
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-03-07 11:00:28 -0500

    Implemented post pages

--- myblog/blogengine/tests.py
+++ myblog/blogengine/tests.py
@@ -11,6 +11,7 @@ class PostTest(TestCase):
 		post = Post()
 		post.title = "My first post"
 		post.text = "This is my first blog post"
+		post.slug = "my-first-post"
 		post.pub_date = timezone.now()
 		post.save()
 
@@ -26,6 +27,8 @@ class PostTest(TestCase):
 
 		self.assertEquals(only_post.title, "My first post")
 		self.assertEquals(only_post.text, 'This is my first blog post')
+		self.assertEquals(only_post.slug, "my-first-post")
+
 		self.assertEquals(only_post.pub_date.day, post.pub_date.day)
 		self.assertEquals(only_post.pub_date.month, post.pub_date.month)
 		self.assertEquals(only_post.pub_date.year, post.pub_date.year)
@@ -117,7 +120,9 @@ class AdminTest(LiveServerTestCase):
 			'title': "My first post",
 			'text' : "This is my first post",
 			'pub_date_0': '2015-03-05',
-			'pub_date_1': '22:00:04'
+			'pub_date_1': '22:00:04',
+			'slug': 'my-first-post'
+
 
 		},
 		follow=True
@@ -154,6 +159,7 @@ class AdminTest(LiveServerTestCase):
 		# 	'text': 'This is my second blog post',
 		# 	'pub_date_0': '2013-12-28',
 		# 	'pub_date_1': '22:00:04',
+		# 	'slug': 'my-first-post'
 		# },
 		# follow=True
 		# )
@@ -206,6 +212,7 @@ class PostViewTesT(LiveServerTestCase):
 		post = Post()
 		post.title = 'My first post'
 		post.text = 'This is my first blog post'
+		post.slug = "my-first-post"
 		post.pub_date = timezone.now()
 		post.save()
 
@@ -259,3 +266,40 @@ class PostViewTest(LiveServerTestCase):
 
 		# Check the link is marked up properly
 		self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
+		def test_post_page(self):
+			# Create the post
+			post = Post()
+			post.title = 'My first post'
+			post.text = 'This is [my first blog post](http://127.0.0.1:8000/)'
+			post.pub_date = timezone.now()
+			post.save()
+
+			# Check new post saved
+			all_posts = Post.objects.all()
+			self.assertEquals(len(all_posts), 1)
+			only_post = all_posts[0]
+			self.assertEquals(only_post, post)
+
+			# Get the post URL
+			post_url = only_post.get_absolute_url()
+
+			# Fetch the post
+			response = self.client.get(post_url)
+			self.assertEquals(response.status_code, 200)
+
+			# Check the post title is in the response
+			self.assertTrue(post.title in response.content)
+
+			# Check the post text is in the response
+			self.assertTrue(markdown.markdown(post.text) in response.content)
+
+			# Check the post date is in the response
+			self.assertTrue(str(post.pub_date.year) in response.content)
+			self.assertTrue(post.pub_date.strftime('%b') in response.content)
+			self.assertTrue(str(post.pub_date.day) in response.content)
+
+			# Check the link is marked up properly
+			self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
+
--- myblog/blogengine/urls.py
+++ myblog/blogengine/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blogengine.models import Post
 
 urlpatterns = patterns('',
@@ -8,4 +8,13 @@ urlpatterns = patterns('',
         model=Post,
         paginate_by=5,
         )),
+
+    # Individual posts
+    url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(
+        model=Post,
+        )),
+
+
+
+
 )
