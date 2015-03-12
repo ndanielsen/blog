a4ff651012ba9f7d0d5fbc87c7d0edaee8346b45
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-03-12 08:20:01 -0400

    Categories are now shown

--- myblog/blogengine/tests.py
+++ myblog/blogengine/tests.py
@@ -425,19 +425,25 @@ class PostViewTesT(BaseAcceptanceTest):
 		post.category = category
 		post.save()
 
+		#Check the new post
 		all_posts = Post.objects.all()
-
 		self.assertEquals(len(all_posts), 1)
 
+		#Fetch the index
 		response = self.client.get("/")
 		self.assertEquals(response.status_code, 200)
 
+		#Check title in post
 		self.assertTrue(post.title in response.content)
 
+		# check post text is there
 		self.assertTrue(markdown.markdown(post.text) in response.content)
 
+		#check post category in the response
 		self.assertTrue(str(post.pub_date.year) in response.content)
 
+		#check post date is in the response
+		self.assertTrue(str(post.pub_date.year) in response.content)
 		self.assertTrue(post.pub_date.strftime('%b') in response.content)
 		self.assertTrue(str(post.pub_date.day) in response.content)
 
@@ -453,14 +459,7 @@ class PostViewTesT(BaseAcceptanceTest):
 		category.description = 'The Python programming language'
 		category.save()
 
-
-
-		# Create the site
-		site = Site()
-		site.name = 'example.com'
-		site.domain = 'example.com'
-		site.save()
-
+		#Create the author
 
 		author = User.objects.create_user('testuser', 'user@example.com', 'password')
 		author.save()
@@ -470,6 +469,7 @@ class PostViewTesT(BaseAcceptanceTest):
 		site.name = 'example.com'
 		site.domain = 'example.com'
 		site.save()
+
 
 		# Create the post
 		post = Post()
@@ -498,6 +498,9 @@ class PostViewTesT(BaseAcceptanceTest):
 		# Check the post title is in the response
 		self.assertTrue(post.title in response.content)
 
+		# Check the post category is in the response
+		self.assertTrue(post.category.name in response.content)
+
 		# Check the post text is in the response
 		self.assertTrue(markdown.markdown(post.text) in response.content)
 
@@ -508,6 +511,86 @@ class PostViewTesT(BaseAcceptanceTest):
 
 		# Check the link is marked up properly
 		self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
+	def test_category_page(self):
+		
+		#Create	 category
+		category = Category()
+		category.name = 'python'
+		category.description = 'The Python programming language'
+		category.save()
+
+		#Create the author
+
+		author = User.objects.create_user('testuser', 'user@example.com', 'password')
+		author.save()
+
+		# Create the site
+		site = Site()
+		site.name = 'example.com'
+		site.domain = 'example.com'
+		site.save()
+
+
+		# Create the post
+		post = Post()
+		post.title = 'My first post'
+		post.text = 'This is [my first blog post](http://127.0.0.1:8000/)'
+		post.slug = "my-first-post"
+		post.pub_date = timezone.now()
+		post.author = author
+		post.site = site
+		post.category = category
+		post.save()
+
+		# Check new post saved
+		all_posts = Post.objects.all()
+		self.assertEquals(len(all_posts), 1)
+		only_post = all_posts[0]
+		self.assertEquals(only_post, post)
+
+		# Get the category URL
+		category_url = post.category.get_absolute_url()
+
+		# Fetch the category
+		response = self.client.get(category_url)
+		self.assertEquals(response.status_code, 200)
+
+
+
+		# Check the category name is in the response
+		self.assertTrue(post.category.name in response.content)
+
+
+		# Check the post text is in the response
+		self.assertTrue(markdown.markdown(post.text) in response.content)
+
+		# Check the post date is in the response
+		self.assertTrue(str(post.pub_date.year) in response.content)
+		self.assertTrue(post.pub_date.strftime('%b') in response.content)
+		self.assertTrue(str(post.pub_date.day) in response.content)
+
+		# Check the link is marked up properly
+		self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class FlatPageViewTest(BaseAcceptanceTest):
